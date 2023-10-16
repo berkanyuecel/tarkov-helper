94a7dd891137427beee7b29ace6e8058959e767c
Berkan Yücel <berkanyuecel@outlook.com>
2023-10-16 21:32:43 +0200

    project initilization

---
+++ src/main/java/byuecel/labor/tarkovhelper/TarkovHelperApplication.java
@@ -0,0 +1,13 @@
+package byuecel.labor.tarkovhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkovHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkovHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/byuecel/labor/tarkovhelper/TarkovHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package byuecel.labor.tarkovhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarkovHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
